13f2bd75e1fb91b54cd0bd30c175673742389e69
Kate <mitsutanaka29@gmail.com>
2023-11-07 23:53:26 +0300

    added menu background

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,6 @@
 var close = document.getElementsByClassName('header__close')[0];
 var burger = document.getElementsByClassName('header__burger')[0];
-var nav = document.getElementsByClassName('header__nav')[0];
+var nav = document.getElementsByClassName('header__wrapper')[0];
 close.onclick = function () {
     nav.classList.add('header__none');
 }
