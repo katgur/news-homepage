dcc8e34323489925c5efe838948a33fbefd199ef
Kate <mitsutanaka29@gmail.com>
2023-11-07 19:45:02 +0300

    added semantic markup

--- gulpfile.js
+++ gulpfile.js
@@ -48,9 +48,9 @@ function images() {
 }
 
 function webp() {
-    return src("dist/img/*.{png,jpg}")
+    return src("dist/images/*.{png,jpg}")
         .pipe(convertwebp({ quality: 90 }))
-        .pipe(dest("dist/img/"));
+        .pipe(dest("dist/images/"));
 }
 
 function scripts() {
